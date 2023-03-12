ca3696dc05448bf4a5d848386434f3f17c2f76ac
Enzo <enzoquelenis@gmail.com>
2023-03-12 15:53:34 +0100

    add method

--- app/components/core.py
+++ app/components/core.py
@@ -4,12 +4,23 @@ import datetime
 import math
 import random
 import time
+import requests
 
 
 def readFileFrom(path=None):
     if None != path:
         return open(path, 'r').readlines()
 
+def get_id_by_questionName(questionName, WOOCLAP_ID, TOKEN):
+    BEARER = f"bearer {TOKEN}"
+
+    RESPONSE = requests.get(f"https://app.wooclap.com/api/events/{WOOCLAP_ID}", headers={ "authorization": BEARER }).json()
+    for response in RESPONSE["questions"]:
+        try:
+            if (questionName == response["title"]):
+                return response["_id"]
+        except:
+            continue
 
 def generate_token():
     presentDate = datetime.datetime.now()
